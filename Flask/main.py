0c2947564a7e4cc8615937793660a8b3b682275a
Mukul1405 <mukulaggarwal1234@gmail.com>
2022-11-22 21:51:04 +0530

    Day-2 mongoApi and Dockerfile corresponding

--- Flask/main.py
+++ Flask/main.py
@@ -1,4 +1,6 @@
-from flask import Flask, request
+from flask import Flask
+import requests
+
 
 app = Flask('__main__')
 
@@ -6,6 +8,15 @@ app = Flask('__main__')
 def home():
     return "Home Page"
 
+@app.route("/users",methods = ["GET"])
+def listUsers():
+    resp = requests.get("0.0.0.0:5002/list")    
+    return resp
+
+@app.route("/adduser/<user>",methods = ["POST"])
+def addUser():
+    resp = requests.post("0.0.0.0:5002/adduser/user")
+    return resp
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0',port=5001,debug=True)
